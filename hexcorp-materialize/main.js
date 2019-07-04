027fe36cb0e78c3f16a41edc60aa2986fc9f8e93
AlysCole <alyscole@outlook.com>
2019-07-04 11:08:25 +0800

    Add animation

--- hexcorp-materialize/main.js
+++ hexcorp-materialize/main.js
@@ -1,4 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
   const elements = document.querySelectorAll('.sidenav');
   const instances = M.Sidenav.init(elements);
+
+  // Enable animation
+  const welcomeSlogan = document.querySelector('.welcome__slogan');
+  const welcomeButton = document.querySelector('.welcome__button');
+
+  welcomeSlogan.className = welcomeSlogan.className + ' welcome__slogan--animated';
+
+  welcomeButton.className = welcomeButton.className + ' welcome__button--animated';
 });
